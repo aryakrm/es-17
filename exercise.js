fb80b60b6ee305d4fd9532ae10df9ccf74a44c79
Arya Karimi <devaryakrm@gmal.com>
2022-09-16 23:38:20 +0300

    codes added

---
+++ exercise.js
@@ -0,0 +1,7 @@
+let isSunnyDay = true;
+let isHomeworkCompleted = false;
+if (isSunnyDay && isHomeworkCompleted == true) {
+  console.log("Jesse can go out to play");
+} else {
+  console.log("Jesse stays home");
+}
